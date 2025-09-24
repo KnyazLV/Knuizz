d886036965a94e87ee31aae6523a6f9b6a3a5696
KnyazLV <r.belovs@inbox.lv>
2025-09-24 18:06:58 +0300

    ErrorPage

--- src/knuizz-web/src/app/routes.tsx
+++ src/knuizz-web/src/app/routes.tsx
@@ -1,5 +1,4 @@
-﻿// src/app/routes.tsx
-import { createBrowserRouter } from "react-router-dom";
+﻿import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import AuthPage from "../pages/AuthPage.tsx";
 import MainLayout from "../components/layout/MainLayout.tsx";
@@ -8,10 +7,12 @@ import LeaderboardPage from "../pages/LeaderboardPage.tsx";
 import ProfilePage from "../pages/ProfilePage.tsx";
 import GameRouteGuard from "../components/feature/game/GameRouteGuard.tsx";
 import GamePage from "../pages/GamePage.tsx";
+import ErrorPage from "@/pages/ErrorPage.tsx";
 
 export const router = createBrowserRouter([
   {
     element: <MainLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
@@ -38,6 +39,7 @@ export const router = createBrowserRouter([
   },
   {
     element: <AuthLayout />,
+    errorElement: <ErrorPage />, // также можете добавить сюда
     children: [
       {
         path: "/auth",
---
+++ src/knuizz-web/src/pages/ErrorPage.tsx
@@ -0,0 +1,55 @@
+﻿import { useRouteError } from "react-router-dom";
+import { Callout, Flex, Text, Button } from "@radix-ui/themes";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
+import { useNavigate } from "react-router-dom";
+
+const getErrorMessage = (error: unknown): string => {
+  if (error && typeof error === "object") {
+    if ("statusText" in error && typeof error.statusText === "string") {
+      return error.statusText;
+    }
+    if ("message" in error && typeof error.message === "string") {
+      return error.message;
+    }
+  }
+  return "Unknown error";
+};
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  const navigate = useNavigate();
+
+  const errorMessage = getErrorMessage(error);
+
+  return (
+    <Flex
+      align="center"
+      // justify="center"
+      direction="column"
+      style={{ height: "100vh", padding: 24, marginTop: 100 }}
+    >
+      <Callout.Root color="red">
+        <Flex direction="column" justify="center" align="center" gap="3">
+          <Flex gap="2" justify="center" align="center">
+            <Callout.Icon>
+              <InfoCircledIcon
+                style={{ width: 35, height: 35, flexShrink: 0 }}
+              />
+            </Callout.Icon>
+
+            <Text size="7" weight="bold">
+              Oops! Something went wrong.
+            </Text>
+          </Flex>
+
+          <Flex direction="column" justify="center" align="center" gap="2">
+            <Callout.Text>{errorMessage}</Callout.Text>
+            <Button variant="solid" onClick={() => navigate("/")}>
+              Go to Home
+            </Button>
+          </Flex>
+        </Flex>
+      </Callout.Root>
+    </Flex>
+  );
+}
